ff575d26787f587f341c290f36ea15302ba5759d
Mikołaj Szczepek <m.szczepek01@gmail.com>
2024-11-09 21:45:19 +0100

    Create admin layout, navigation

---
+++ Wojcik.Client/Components/AdminNavigation.razor.cs
@@ -0,0 +1,37 @@
+﻿using Radzen;
+using System.Security.Claims;
+
+namespace Wojcik.Client.Components;
+
+public partial class AdminNavigation
+{
+	private ClaimsPrincipal user = new();
+	private MenuItemDisplayStyle DisplayStyle = MenuItemDisplayStyle.IconAndText;
+	private bool isShowArrow = true;
+	private string sidebar = "sidebar-expanded";
+	private string subSidebar = "sub-sidebar-expanded";
+
+	protected override async Task OnInitializedAsync()
+	{
+		var authState = await authStateProvider.GetAuthenticationStateAsync();
+		this.user = authState.User;
+	}
+
+	private void ToggleMenu()
+	{
+		if (DisplayStyle == MenuItemDisplayStyle.IconAndText)
+		{
+			DisplayStyle = MenuItemDisplayStyle.Icon;
+			isShowArrow = false;
+			sidebar = "sidebar-collapsed";
+			subSidebar = "sub-sidebar-collapsed";
+		}
+		else
+		{
+			DisplayStyle = MenuItemDisplayStyle.IconAndText;
+			isShowArrow = true;
+			sidebar = "sidebar-expanded";
+			subSidebar = "sub-sidebar-expanded";
+		}
+	}
+}
---
+++ Wojcik.Client/Layouts/MainLayout.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components;
+
+namespace Wojcik.Client.Layouts;
+
+public partial class MainLayout
+{
+	[CascadingParameter]
+	Task<AuthenticationState> AuthenticationState { get; set; } = default!;
+
+	protected override async Task<Task> OnInitializedAsync()
+	{
+		if (!(await AuthenticationState).User.Identity!.IsAuthenticated)
+		{
+			navigationManager.NavigateTo("/login");
+		}
+
+		return base.OnInitializedAsync();
+	}
+}
